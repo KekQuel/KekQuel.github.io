eb9cda4b4e940387b8ce9b7344f4b50c76ef947b
KekQuel <wilkinschaunj@gmail.com>
2026-03-05 17:01:07 +0800

    Update HomePage.tsx

--- nyp_portfolio/src/pages/HomePage.tsx
+++ nyp_portfolio/src/pages/HomePage.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
